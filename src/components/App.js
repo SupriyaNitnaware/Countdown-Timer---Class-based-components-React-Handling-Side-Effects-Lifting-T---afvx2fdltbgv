ba101c4598896d2f6c3bbec38b520187aaaa4441
Supriya Nitnaware <80514079+SupriyaNitnaware@users.noreply.github.com>
2023-02-27 15:57:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,14 @@
 import React, { createContext, useState } from "react";
 
-import "../styles/App.css";
-import Comp2 from "./Comp2";
-const nameContext = createContext();
 
 const App = () => {
-    const [state,setStates] = useState("")
+   
   return (
     <div id="main">
-    <nameContext.Provider value={state}>
-      <input id="input" onChange={(e)=>{setStates(e.target.value)}}/>
-      <Comp2 />
-      </nameContext.Provider>
+   
     </div>
-  );
-};
+  )
+}
 
 export default App;
-export { nameContext };
+
